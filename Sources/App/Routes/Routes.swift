de7e577c88d4ad346eeaef35e202bce5a2547d40
Elias Salokivi <salokive@Glittertind-5.local>
2017-11-29 20:20:07 +0200

    first one

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -9,7 +9,7 @@ extension Droplet {
         }
 
         get("plaintext") { req in
-            return "Hello, world!"
+            return "Hello, world"
         }
 
         // response to requests to /info domain
